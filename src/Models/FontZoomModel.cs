5b41c4d52aca311fbe31bf851ecc0c03363eac26
savaged <savagedavid@gmail.com>
2019-04-16 17:23:33 +0100

    working on zoom

---
+++ src/Models/FontZoomModel.cs
@@ -0,0 +1,13 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace Savaged.BlackNotepad.Models
+{
+    public class FontZoomModel : ObservableObject
+    {
+        public int Key { get; }
+
+        public int FontSize { get; }
+
+        public int Zoom { get; }
+    }
+}

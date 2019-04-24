3d445ace790099d0f3a0f1f27a753b024269d4f8
savaged <savagedavid@gmail.com>
2019-04-24 14:54:53 +0100

    first UI test

---
+++ BlackNotepad.Test/UITests/BasicTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+using TestStack.White;
+using TestStack.White.UIItems.MenuItems;
+
+namespace BlackNotepad.Test.UITests
+{
+    [TestClass]
+    public class BasicTests : UITestBase
+    {
+        [TestMethod]
+        public void StartupTest()
+        {
+            Assert.IsNotNull(MainWindow);
+            var menuBar = MainWindow.MenuBar;
+            Assert.IsNotNull(menuBar);
+            Thread.Sleep(100);
+            Menu menuItem = null;
+            try
+            {
+                menuItem = menuBar.MenuItem("Help", "About");
+            }
+            catch (AutomationException ex)
+            {
+                Assert.Fail(
+                    "The menu item is not enabled! this may be due to " +
+                    "slow running. Try increasing the delay above. " +
+                    $"Exception: {ex.Message}");
+            }
+            Assert.IsNotNull(menuItem);
+            menuItem.Click();
+
+            var windows = App.GetWindows();
+            Assert.AreEqual(2, windows.Count);
+            var win = windows[1];
+            Assert.IsNotNull(win);
+            Assert.AreEqual("About", win.Title);
+            win.Close();
+            // TODO loads more to test like this. May also need to add Name properties to many elements
+        }
+    }
+}
---
+++ BlackNotepad.Test/UITests/UITestBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.White;
+using TestStack.White.UIItems.WindowItems;
+
+namespace BlackNotepad.Test.UITests
+{
+    [TestClass]
+    public abstract class UITestBase
+    {
+        protected Application App { get; private set; }
+
+        protected Window MainWindow { get; private set; }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var outputDir = new DirectoryInfo(
+                    Assembly.GetExecutingAssembly().Location).Parent.FullName;
+            var appName = $"{Assembly.GetExecutingAssembly().GetName()}"
+                .Replace("Test", "exe");
+            appName = appName.Remove(appName.IndexOf(','));
+            var location = $"{outputDir}\\{appName}";
+            App = Application.Launch(location);
+
+            MainWindow = App.GetWindows().FirstOrDefault();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            App.Close();
+            App.Dispose();
+        }
+    }
+}
